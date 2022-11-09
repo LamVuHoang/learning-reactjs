9cd2402f8e998fbc4fb2a03ea9829bb8f3f8bb9a
Lam Vu Hoang <lam.vuhoang@nashtechglobal.com>
2022-11-09 10:31:15 +0700

    fix button style

--- src/UI/SubmitButton.jsx
+++ src/UI/SubmitButton.jsx
@@ -3,7 +3,7 @@ export function SubmitButton(props) {
     <div className={props.margin}>
       <button
         type="submit"
-        className="inline-block px-7 py-3 bg-indigo-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        className={`${props.disabled ? "bg-gray-300 hover:bg-gray-200 focus:bg-gray-200 active:bg-gray-100" : "bg-indigo-600 hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-800"} inline-block px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md  hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out`}
         disabled={props.disabled}
       >
         {props.label}
