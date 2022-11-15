ff7742297aa7baf5c9782cd65e94fd1a2fa1942a
Lam Vu Hoang <lam.vuhoang@nashtechglobal.com>
2022-11-15 08:30:54 +0700

    fix(fe) useCallback, useEffect and axios

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -3,44 +3,46 @@ import { authContext } from "../../Store/auth-context";
 import axios from "axios";
 export default function Index() {
   const ctx = useContext(authContext);
-  // const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState(undefined);
-  // const [error, setError] = useState(undefined);
-  const [content, setContent] = useState("");
+  const [error, setError] = useState(undefined);
 
   const fetchUser = useCallback(async () => {
-    // setIsLoading(true);
-    // try {
-    const response = await axios.get(
-      `https://swapi.dev/api/people/${window.localStorage.getItem("token")}`
-    );
+    
+    // Correct result
+    // const url = `https://swapi.dev/api/people/${window.localStorage.getItem("token")}`
 
-    // if (response.code !== 200) {
-    //   setError(response.message);
-    //   throw new Error("Some thing wrong");
-    // }
+    // Wrong result
+    const url = `https://swapi.dev/api/peoples/${window.localStorage.getItem("token")}`;
 
-    setUser(response.data);
-    // } catch (errors) {
-    //   throw new Error("Some thing wrong");
-    // }
+    setIsLoading(true);
+    try {
+      const response = await axios.get(url);
 
-    // setIsLoading(false);
-  }, [user]);
+      if (response.code !== 200) {
+        setError(response.message);
+      }
+
+      setUser(response.data);
+    } catch (errors) {
+      setError(errors);
+    }
+
+    setIsLoading(false);
+  }, []);
 
   useEffect(() => {
     fetchUser();
   }, [fetchUser]);
 
-  // if (isLoading) {
-  //   setContent("Loading ...");
-  // }
-  if (user) {
-    setContent(user);
+  let content = "No data User";
+  if (isLoading) {
+    content = "Loading ...";
+  } else if (user) {
+    content = user.name;
+  } else {
+    content = error.message;
   }
-  //  else {
-  //   setContent(error);
-  // }
 
   const onLogout = () => {
     window.localStorage.removeItem("token");
@@ -51,7 +53,7 @@ export default function Index() {
     <>
       <div className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
         {content}
-
+        <br />
         <button
           type="button"
           className="bg-indigo-600 text-white p-2 font-bold rounded-md"
